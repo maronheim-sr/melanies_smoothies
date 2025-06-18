831b5826c36508d79993f956660ec536ce9d67de
maronheim-sr <maronheim@spauldingridge.com>
2025-06-18 09:29:20 -0500

    Update streamlit_app.py
    code changes to assist SiS to SniS deployment transition

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -8,6 +8,8 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be", name_on_order)
 
+cnx = st.connection("snowflake")
+session = cnx.session()
 session = get_active_session()
 
 # Get fruit options
